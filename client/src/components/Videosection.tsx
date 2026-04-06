8aec20d32a63950cef2977c940e211be683f7b5c
certifyied <soorajcpherupunna2@gmail.com>
2026-04-06 17:05:38 +0530

    Changed name

--- client/src/components/Videosection.tsx
+++ client/src/components/Videosection.tsx
@@ -9,7 +9,7 @@ function Videosection() {
                 {/* LEFT - Video */}
                 <div className="relative group w-full rounded-3xl overflow-hidden shadow-2xl">
                     <video
-                        src="/public/PENDORA REEL.mp4"
+                        src="/public/pendora-reel.mp4"
                         autoPlay
                         loop
                         muted
